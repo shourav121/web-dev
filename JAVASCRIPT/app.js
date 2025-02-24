e01e5a9f6142a05e5285f9a79f897d86cc1b1c29
shourav kumar <shouravk81@gmail.com>
2025-02-24 23:13:21 +0530

    work

---
+++ JAVASCRIPT/app.js
@@ -0,0 +1,36 @@
+let name="shourav kumar";
+console.log(name)
+
+let u=10;
+let p=5;
+console.log("sun of a+b is" ,u+p);
+
+let pencilPrice =10;
+let eraserPrice=20;
+//!let output=("sun of two is: "+(pencilPrice+eraserPrice)+ " " +"rupees");
+
+
+//! Template--------------------------litterals::---------------------------------------------
+
+let output=`the total price is: ${pencilPrice+eraserPrice} rupees`;
+console.log(output); 
+
+//!arithmeticOperators-- ----------------------------
+
+let a=8;
+let b=2;
+console.log(a+b);
+console.log(a-b);
+console.log(a/b);
+console.log(a*b);
+console.log(a%b);
+
+let j=10;
+let k=5;
+console.log(j++);
+console.log(++j);
+
+//!Comparision Operator------------------------------------------------
+
+let age =18;
+console.log(age>=18);
