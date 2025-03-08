a96eb19ab36d9de873652127c539ab1ad181a43f
shourav kumar <shouravk81@gmail.com>
2025-03-08 21:19:35 +0530

    work

--- JAVASCRIPT/app.js
+++ JAVASCRIPT/app.js
@@ -34,3 +34,37 @@ console.log(++j);
 
 let age =18;
 console.log(age>=18);
+
+//sum of all n number i.e:- 1+2+3+4+5+6+7............
+function getSum(n){
+    sum = 0;
+    for(let i=1;i<=n;i++){
+        sum=sum+i
+    }
+    return sum;
+}
+
+
+//concat add all laeter=============
+
+let arr=["sdsd","Sdsds","Ssfs","sfssf","dfsdfsd"];
+
+function concat(arr){
+    let finalanswer="";
+    for(i=0;i<=arr.length;i++){
+        finalanswer+=arr[i];
+    }
+
+    return finalanswer;
+}
+
+//lexial  scope--------
+
+function outer(){
+    let a=2;
+    let b=7;
+    function inner(){
+        console.log(b);
+    }
+    inner();
+}
