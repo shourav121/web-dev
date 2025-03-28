073445c558310dc2a4526c802770eb4916448993
shourav kumar <shouravk81@gmail.com>
2025-03-28 22:07:07 +0530

    work

--- JAVASCRIPT/day 22/app.js
+++ JAVASCRIPT/day 22/app.js
@@ -1,5 +1,11 @@
 let links = document.querySelectorAll(".box a");
 
-    for(let i=0;i<links.length;i++){
-        links[i].style.color='red';
-    }
+        //!for loop
+
+    // for(let i=0;i<links.length;i++){
+    //     links[i].style.color='red';
+    // }
+        //!FOR OF LOOP
+        for(link of links){
+            link.style.color="green";
+        }
