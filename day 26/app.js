fe9c2b9f8823200f75071f0ebb39dccd278473f1
shourav kumar <shouravk81@gmail.com>
2025-04-19 23:11:56 +0530

    wor

---
+++ day 26/app.js
@@ -0,0 +1,8 @@
+let word=`{"fact":"A cat has more bones than a human being; humans have 206 and the cat has 230 bones.","length":83}`
+let validResp=JSON.parse(word);
+console.log(validResp);
+
+let student = {
+    name: "shourav",
+    rollno: 1323427,
+};
